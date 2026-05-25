efaa25662d2d5b8ee48da3c4621a4348530c6358
Yab Mas <jaapmaaskant@hotmail.com>
2026-05-25 16:12:02 +0200

    feat(canvas): defquery + name-resolution expansion for constraint bodies

---
+++ src/fukan/canvas/defquery.clj
@@ -0,0 +1,65 @@
+(ns fukan.canvas.defquery)
+
+(def registry (atom {}))
+
+(def ^:private primitive-kinds #{'Module 'Affordance 'State 'Type})
+
+(defn- walk-replace [mapping form]
+  (cond
+    (symbol? form) (get mapping form form)
+    (seq? form)    (with-meta (map #(walk-replace mapping %) form) (meta form))
+    (vector? form) (with-meta (mapv #(walk-replace mapping %) form) (meta form))
+    :else form))
+
+(defn- substitute-vars
+  "Substitute formal parameters in a query body with actual variable names."
+  [body params args]
+  (let [mapping (zipmap params args)]
+    (walk-replace mapping body)))
+
+(defn expand
+  "Recursively expand a sequence of constraint clauses, resolving named forms
+   into datom patterns."
+  [clauses]
+  (vec
+    (mapcat
+      (fn [clause]
+        (cond
+          ;; Already a datom pattern (vector): pass through
+          (vector? clause)
+          [clause]
+
+          ;; Named form: (Module ?x), (tag :X ?e), (defquery'd-op args)
+          (seq? clause)
+          (let [head (first clause)
+                args (rest clause)]
+            (cond
+              ;; Primitive type pattern
+              (contains? primitive-kinds head)
+              [[(first args) :entity/type (keyword (str head))]]
+
+              ;; Tag pattern
+              (= head 'tag)
+              [[(second args) :entity/tag (first args)]]
+
+              ;; Registered defquery operator
+              (contains? @registry head)
+              (let [{:keys [params body]} (@registry head)
+                    substituted (substitute-vars body params args)]
+                (expand substituted))
+
+              :else
+              (throw (ex-info (format "unknown query form: %s" head) {:head head}))))
+
+          :else
+          (throw (ex-info "unexpected clause type" {:clause clause}))))
+      clauses)))
+
+(defmacro defquery
+  "Register a named Datalog operator. The body is a Datalog query body
+   that may reference other defquery'd operators."
+  [name params docstring body]
+  `(swap! registry assoc '~name
+          {:params '~params
+           :doc ~docstring
+           :body ~body}))
---
+++ test/fukan/canvas/defquery_test.clj
@@ -0,0 +1,32 @@
+(ns fukan.canvas.defquery-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [fukan.canvas.defquery :as dq]))
+
+(deftest primitive-name-expansion
+  (testing "(Module ?x) expands to [?x :entity/type :Module]"
+    (is (= '[[?x :entity/type :Module]]
+           (dq/expand '[(Module ?x)])))))
+
+(deftest tag-name-expansion
+  (testing "(tag :X ?e) expands to [?e :entity/tag :X]"
+    (is (= '[[?e :entity/tag :X]]
+           (dq/expand '[(tag :X ?e)])))))
+
+(deftest defquery-registers-and-expands
+  (testing "a defquery'd operator expands into its body"
+    (dq/defquery test-op [?x ?y]
+      "Test operator."
+      '[[?x :foo ?y]])
+    (is (= '[[?a :foo ?b]]
+           (dq/expand '[(test-op ?a ?b)])))))
+
+(deftest recursive-expansion
+  (testing "defquery bodies can reference other defquery'd operators"
+    (dq/defquery base-op [?x]
+      "base"
+      '[[?x :entity/type :Module]])
+    (dq/defquery outer-op [?x]
+      "outer"
+      '[(base-op ?x) [?x :entity/name ?n]])
+    (is (= '[[?a :entity/type :Module] [?a :entity/name ?n]]
+           (dq/expand '[(outer-op ?a)])))))
