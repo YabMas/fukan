a7ca1fe90b23383eb6303813ad1ad72871833cee
Yab Mas <jaapmaaskant@hotmail.com>
2026-05-25 15:51:50 +0200

    feat(canvas): substrate store — Datascript-backed transact/query API

---
+++ src/fukan/canvas/substrate/store.clj
@@ -0,0 +1,68 @@
+(ns fukan.canvas.substrate.store
+  (:require [datascript.core :as d]
+            [fukan.canvas.substrate :as sub]))
+
+(def ^:private schema
+  {:entity/id           {:db/unique :db.unique/identity}
+   :entity/type         {:db/index true}
+   :entity/name         {:db/index true}
+   :affordance/module   {:db/valueType :db.type/ref}
+   :state/module        {:db/valueType :db.type/ref}
+   :module/child        {:db/cardinality :db.cardinality/many
+                         :db/valueType :db.type/ref}
+   :entity/tag          {:db/cardinality :db.cardinality/many}})
+
+(defn create []
+  (d/empty-db schema))
+
+(defmulti ^:private ->datoms sub/primitive-kind)
+
+(defmethod ->datoms :Module [m]
+  [{:entity/id (sub/id-of m)
+    :entity/type :Module
+    :entity/name (sub/name-of m)
+    :entity/tag (vec (sub/tags-of m))}])
+
+(defmethod ->datoms :Affordance [a]
+  [(cond-> {:entity/id (sub/id-of a)
+            :entity/type :Affordance
+            :entity/name (sub/name-of a)
+            :entity/tag (vec (sub/tags-of a))}
+     (sub/module-of a)
+     (assoc :affordance/module [:entity/id (sub/module-of a)])
+     (sub/role-of a)
+     (assoc :affordance/role (sub/role-of a))
+     (sub/shape-of a)
+     (assoc :affordance/shape (pr-str (sub/shape-of a))))])
+
+(defmethod ->datoms :State [s]
+  [{:entity/id (sub/id-of s)
+    :entity/type :State
+    :entity/name (sub/name-of s)
+    :state/module [:entity/id (sub/module-of s)]
+    :state/shape (sub/shape-of s)
+    :entity/tag (vec (sub/tags-of s))}])
+
+(defmethod ->datoms :Type [t]
+  [{:entity/id (sub/id-of t)
+    :entity/type :Type
+    :entity/name (sub/name-of t)
+    :entity/tag (vec (sub/tags-of t))}])
+
+(defmethod ->datoms :Relation [r]
+  [[:db/add [:entity/id (sub/from-of r)] (sub/kind-of r) [:entity/id (sub/to-of r)]]])
+
+(defn transact! [db entity]
+  (d/db-with db (->datoms entity)))
+
+(defn all-modules [db]
+  (->> (d/q '[:find ?n :where [?e :entity/type :Module] [?e :entity/name ?n]] db)
+       (map (fn [[n]] {:name n}))))
+
+(defn affordances-in [db module-id]
+  (d/q '[:find ?n
+         :in $ ?mid
+         :where [?m :entity/id ?mid]
+                [?a :affordance/module ?m]
+                [?a :entity/name ?n]]
+       db module-id))
---
+++ test/fukan/canvas/store_test.clj
@@ -0,0 +1,26 @@
+(ns fukan.canvas.store-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [fukan.canvas.substrate :as sub]
+            [fukan.canvas.substrate.store :as store]))
+
+(deftest store-creation
+  (testing "creates an empty store"
+    (let [s (store/create)]
+      (is (some? s))
+      (is (empty? (store/all-modules s))))))
+
+(deftest transact-module
+  (testing "adds a Module and finds it"
+    (let [s (-> (store/create)
+                (store/transact! (sub/module "accounts")))]
+      (is (= 1 (count (store/all-modules s))))
+      (is (= "accounts" (-> s store/all-modules first :name))))))
+
+(deftest transact-affordance-with-module
+  (testing "an Affordance references its containing Module by id"
+    (let [m (sub/module "accounts")
+          a (sub/affordance "create" :module (sub/id-of m))
+          s (-> (store/create)
+                (store/transact! m)
+                (store/transact! a))]
+      (is (= 1 (count (store/affordances-in s (sub/id-of m))))))))
