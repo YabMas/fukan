0ffd3321efe5c3971964421d4c8a455f4dc74ea4
Yab Mas <jaapmaaskant@hotmail.com>
2026-05-25 15:49:07 +0200

    feat(canvas): substrate primitives — Module, Affordance, State, Type, Relation, Tag

---
+++ src/fukan/canvas/substrate.clj
@@ -0,0 +1,55 @@
+(ns fukan.canvas.substrate
+  "Six substrate primitives: Module, Affordance, State, Type, Relation, Tag.
+   Architecture-neutral; ships zero role/relation/tag vocabulary.")
+
+(defn- gen-id [] (random-uuid))
+
+(defrecord Module       [id name children tags])
+(defrecord Affordance   [id name module shape role formal-expression tags])
+(defrecord State        [id name module shape tags])
+(defrecord Type         [id name kind fields tags])
+(defrecord Relation     [from kind to tags])
+
+(defn module [name]
+  (->Module (gen-id) name #{} #{}))
+
+(defn affordance [name & {:keys [module shape role formal-expression]}]
+  (->Affordance (gen-id) name module shape role formal-expression #{}))
+
+(defn state [name & {:keys [module shape]}]
+  (when-not shape
+    (throw (ex-info "State requires :shape" {:name name})))
+  (->State (gen-id) name module shape #{}))
+
+(defn type-primitive [name]
+  (->Type (gen-id) name :atomic nil #{}))
+
+(defn type-record [name fields]
+  (->Type (gen-id) name :record fields #{}))
+
+(defn relation [from kind to]
+  (when-not (keyword? kind)
+    (throw (ex-info "Relation kind must be a keyword" {:kind kind})))
+  (->Relation from kind to #{}))
+
+(defn apply-tag [entity tag]
+  (update entity :tags conj tag))
+
+;; Accessors
+(defn id-of [e] (or (:id e) (when (instance? Relation e) [(:from e) (:kind e) (:to e)])))
+(defn name-of [e] (:name e))
+(defn module-of [e] (:module e))
+(defn role-of [e] (:role e))
+(defn shape-of [e] (:shape e))
+(defn from-of [r] (:from r))
+(defn kind-of [r] (:kind r))
+(defn to-of [r] (:to r))
+(defn tags-of [e] (:tags e))
+
+(defn primitive-kind [e]
+  (cond
+    (instance? Module e) :Module
+    (instance? Affordance e) :Affordance
+    (instance? State e) :State
+    (instance? Type e) :Type
+    (instance? Relation e) :Relation))
---
+++ test/fukan/canvas/substrate_test.clj
@@ -0,0 +1,46 @@
+(ns fukan.canvas.substrate-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [fukan.canvas.substrate :as sub]))
+
+(deftest module-construction
+  (testing "a Module has a name and an id"
+    (let [m (sub/module "accounts")]
+      (is (= "accounts" (sub/name-of m)))
+      (is (some? (sub/id-of m)))
+      (is (= :Module (sub/primitive-kind m))))))
+
+(deftest affordance-construction
+  (testing "an Affordance has name, module-id, optional shape/role/formal-expression"
+    (let [m (sub/module "accounts")
+          a (sub/affordance "create-account"
+                            :module (sub/id-of m)
+                            :shape {:inputs :unit :outputs :unit}
+                            :role :exposed-call)]
+      (is (= "create-account" (sub/name-of a)))
+      (is (= (sub/id-of m) (sub/module-of a)))
+      (is (= :exposed-call (sub/role-of a)))
+      (is (= :Affordance (sub/primitive-kind a))))))
+
+(deftest state-construction
+  (testing "a State has name, module-id, required shape (type-ref)"
+    (let [m (sub/module "accounts")
+          ty (sub/type-primitive :String)
+          s (sub/state "config" :module (sub/id-of m) :shape (sub/id-of ty))]
+      (is (= "config" (sub/name-of s)))
+      (is (= :State (sub/primitive-kind s))))))
+
+(deftest relation-construction
+  (testing "a Relation is a directed triple with optional tags"
+    (let [m (sub/module "accounts")
+          a (sub/affordance "x" :module (sub/id-of m))
+          b (sub/affordance "y" :module (sub/id-of m))
+          r (sub/relation (sub/id-of a) :fukan.canvas.monolith/calls (sub/id-of b))]
+      (is (= (sub/id-of a) (sub/from-of r)))
+      (is (= :fukan.canvas.monolith/calls (sub/kind-of r)))
+      (is (= (sub/id-of b) (sub/to-of r))))))
+
+(deftest tag-construction
+  (testing "a Tag applies to an entity"
+    (let [m (sub/module "accounts")
+          t (sub/apply-tag m :Deprecated)]
+      (is (contains? (sub/tags-of t) :Deprecated)))))
