87ebc4a19f6780ab98652215390967ec5e509aba
Yab Mas <jaapmaaskant@hotmail.com>
2026-05-26 14:09:56 +0200

    feat(canvas): port web/views/shell spec

---
+++ test/canvas/web/views/shell_test.clj
@@ -0,0 +1,18 @@
+(ns canvas.web.views.shell-test
+  (:require [clojure.test :refer [deftest is]]
+            [canvas.web.views.shell :as port]
+            [fukan.canvas.core.substrate.store :as store]
+            [datascript.core :as d]))
+
+(deftest canvas-build-produces-non-empty-store
+  (let [db (port/build-canvas)]
+    (is (some? db))
+    (is (>= (count (store/all-modules db)) 1))))
+
+(deftest canvas-build-has-expected-entities
+  (let [db (port/build-canvas)
+        names (->> (d/q '[:find ?n :where [?e :entity/name ?n]] db)
+                   (map first)
+                   set)]
+    (is (contains? names "web.views.shell") "module present")
+    (is (contains? names "render_app_shell") "render_app_shell function present")))
